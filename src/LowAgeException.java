32b9760b0e8fc43975bfaad40e46e7578f9f6bc5
nikdenalb <136702482+NikDenAlb@users.noreply.github.com>
2024-04-14 11:33:56 +0300

    ticket 7

---
+++ src/LowAgeException.java
@@ -0,0 +1,5 @@
+public class LowAgeException extends IllegalArgumentException {
+    public LowAgeException(String message) {
+        super(message);
+    }
+}
---
+++ src/NeedGuardianException.java
@@ -0,0 +1,5 @@
+public class NeedGuardianException extends IllegalArgumentException {
+    public NeedGuardianException(String message) {
+        super(message);
+    }
+}
